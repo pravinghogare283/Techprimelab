b5dc7c96b192c2f9c9d3b4da67265569167db1d2
WaghmodeSS <sachinwaghmode1000@gmail.com>
2023-03-23 18:47:35 +0530

    Dashboard routing added

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  projectList: boolean = false;
+  createProject: boolean = false;
+  graph: boolean = true;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  onAdd() {
+    this.createProject = true;
+    this.projectList = false;
+    this.graph = false;
   }
 
+  onList() {
+    this.createProject = false;
+    this.projectList = true;
+    this.graph = false;
+  }
+
+  changeRoute() {
+    this.createProject = false;
+    this.projectList = false;
+    this.graph = true;
+  }
 }
