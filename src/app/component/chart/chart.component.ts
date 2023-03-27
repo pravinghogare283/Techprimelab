bfb67dbc9e693b2968d9ea8776c57d9472a62099
WaghmodeSS <sachinwaghmode1000@gmail.com>
2023-03-27 10:56:54 +0530

    chart ui improved

--- src/app/component/chart/chart.component.ts
+++ src/app/component/chart/chart.component.ts
@@ -12,30 +12,25 @@ export class ChartComponent implements OnInit {
     this.chart = new Chart("canvas", {
       type: "bar",
       data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: ["STR", "FRN", "OLT", "MAN", "STO", "HR"],
         datasets: [
           {
             label: "# of Votes",
             data: [12, 19, 3, 5, 2, 3],
             backgroundColor: [
-              "rgba(255, 99, 132, 0.2)",
-              "rgba(54, 162, 235, 0.2)",
-              "rgba(255, 206, 86, 0.2)",
-              "rgba(75, 192, 192, 0.2)",
-              "rgba(153, 102, 255, 0.2)",
-              "rgba(255, 159, 64, 0.2)"
-            ],
-            borderColor: [
-              "rgba(255, 99, 132, 1)",
-              "rgba(54, 162, 235, 1)",
-              "rgba(255, 206, 86, 1)",
-              "rgba(75, 192, 192, 1)",
-              "rgba(153, 102, 255, 1)",
-              "rgba(255, 159, 64, 1)"
-            ],
-            borderWidth: 1
+              "blue", "blue", "blue",
+              "blue", "blue", "blue"
+            ]
+          },
+          {
+            label: "# of Votes",
+            data: [15, 10, 6, 10, 4, 6],
+            backgroundColor: [
+              "green", "green", "green",
+              "green", "green", "green"
+            ]
           }
-        ]
+        ],
       },
       options: {
         scales: {
